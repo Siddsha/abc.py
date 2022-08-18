19b4259e4a6635778cf527f4fa95507bb2eacc5f
Siddsha <111046748+Siddsha@users.noreply.github.com>
2022-08-18 21:41:14 +0530

    Add files via upload

---
+++ my_captain.py
@@ -0,0 +1,12 @@
+Python 3.10.6 (v3.10.6:9c7b4bd164, Aug  1 2022, 17:13:48) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+X = ("Input the radius of the circle :")
+Y = "1.1"
+print(X + Y)
+Input the radius of the circle :1.1
+A = ("The area of the circle with radius 1.1 is:")
+B = "3.8013271108436504"
+print(A)
+The area of the circle with radius 1.1 is:
+print(B)
+3.8013271108436504
